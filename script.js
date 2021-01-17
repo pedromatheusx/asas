4838c21a40503cfa64b32230d1a5c881f04faccd
Pedro Matheus <pedromatheusduarte@gmail.com>
2021-01-17 17:57:37 -0300

    forum

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+function load(){
+    window.location.reload(true);
+}
 
 var btnForum = document.getElementById('btn-forum')
 var containerForum = document.getElementById('forum1')
 btnForum.addEventListener('click', function () {
 
   if (containerForum.style.display === 'none') {
+    document.querySelector("#test > div.container > div.bodySearch.col.btn > form > div > label").innerText = 'FÓRUM'
     containerForum.style.display = 'block';
     document.getElementById('vagas').style.display = 'none';
     document.getElementById('rede').style.display = 'none'
@@ -20,6 +24,7 @@ var containerVagas = document.getElementById('vagas')
 btnVagas.addEventListener('click', function () {
 
   if (containerVagas.style.display === 'none') {
+    document.querySelector("#test > div.container > div.bodySearch.col.btn > form > div > label").innerText = 'VAGAS'
     containerVagas.style.display = 'block';
     document.getElementById('forum1').style.display = 'none';
     document.getElementById('rede').style.display = 'none'
@@ -34,6 +39,7 @@ var containerRede = document.getElementById('rede')
 btnRede.addEventListener('click', function () {
 
   if (containerRede.style.display === 'none') {
+    document.querySelector("#test > div.container > div.bodySearch.col.btn > form > div > label").innerText = 'REDE DE ASSISTÊNCIA'
     containerRede.style.display = 'block';
     document.getElementById('forum1').style.display = 'none';
     document.getElementById('vagas').style.display = 'none'
